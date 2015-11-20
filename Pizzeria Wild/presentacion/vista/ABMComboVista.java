4e37f8e13c723bce87853235537976863e5a8758
ESTEBANRIVERA1977 <ESTEBANRIVERA77@GMAIL.COM>
2015-11-20 15:20:35 -0200

    ABM combos se modifico la interfaz. NUNCA tubo FUNCIONALIDAD

--- Pizzeria Wild/presentacion/vista/ABMComboVista.java
+++ Pizzeria Wild/presentacion/vista/ABMComboVista.java
@@ -12,6 +12,9 @@ import javax.swing.table.DefaultTableModel;
 
 import objetosVistaCustom.WDefaultTableModel;
 import objetosVistaCustom.WTable;
+import java.awt.Toolkit;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class ABMComboVista extends JDialog {
 	private JTextField txtIdcombo;
@@ -29,51 +32,56 @@ public class ABMComboVista extends JDialog {
 
 	public ABMComboVista(ComboVista Vista) {
 		super(Vista, true);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(ABMComboVista.class.getResource("/Iconos/pizza_1.PNG")));
 		
-		setTitle("Combo");
+		setTitle(" Combo");
 		setResizable(false);
-		setBounds(100, 100, 649, 452);
+		setBounds(100, 100, 701, 452);
 		getContentPane().setLayout(null);
 		this.setLocationRelativeTo(null);
 		
 		JLabel lblIdCombo = new JLabel("Id Combo: ");
-		lblIdCombo.setBounds(10, 11, 94, 14);
+		lblIdCombo.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblIdCombo.setBounds(10, 8, 94, 25);
 		getContentPane().add(lblIdCombo);
 		
 		txtIdcombo = new JTextField();
-		txtIdcombo.setBounds(89, 8, 86, 20);
+		txtIdcombo.setBounds(89, 8, 86, 25);
 		getContentPane().add(txtIdcombo);
 		txtIdcombo.setColumns(10);
 		
 		JLabel lblDescripcion = new JLabel("Descripcion: ");
-		lblDescripcion.setBounds(10, 39, 94, 14);
+		lblDescripcion.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblDescripcion.setBounds(10, 39, 94, 25);
 		getContentPane().add(lblDescripcion);
 		
 		txtDescripcion = new JTextField();
-		txtDescripcion.setBounds(89, 36, 253, 20);
+		txtDescripcion.setBounds(89, 39, 253, 25);
 		getContentPane().add(txtDescripcion);
 		txtDescripcion.setColumns(10);
 		
 		JLabel lblDesde = new JLabel("Desde:");
-		lblDesde.setBounds(10, 67, 46, 14);
+		lblDesde.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblDesde.setBounds(10, 75, 46, 25);
 		getContentPane().add(lblDesde);
 		
 		txtFechaInicio = new JTextField();
-		txtFechaInicio.setBounds(89, 64, 86, 20);
+		txtFechaInicio.setBounds(89, 75, 86, 25);
 		getContentPane().add(txtFechaInicio);
 		txtFechaInicio.setColumns(10);
 		
 		JLabel lblHasta = new JLabel("Hasta");
-		lblHasta.setBounds(223, 67, 68, 14);
+		lblHasta.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblHasta.setBounds(202, 75, 68, 25);
 		getContentPane().add(lblHasta);
 		
 		txtFechaHasta = new JTextField();
-		txtFechaHasta.setBounds(280, 64, 86, 20);
+		txtFechaHasta.setBounds(249, 75, 86, 25);
 		getContentPane().add(txtFechaHasta);
 		txtFechaHasta.setColumns(10);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 92, 524, 255);
+		scrollPane.setBounds(10, 113, 524, 245);
 		getContentPane().add(scrollPane);
 		
 		this.modelTable = new WDefaultTableModel(null, this.nombreColumnas);
@@ -81,29 +89,43 @@ public class ABMComboVista extends JDialog {
 		scrollPane.setViewportView(table);
 		
 		JLabel lblPrecio = new JLabel("Precio: ");
-		lblPrecio.setBounds(10, 361, 94, 14);
+		lblPrecio.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblPrecio.setBounds(10, 373, 94, 25);
 		getContentPane().add(lblPrecio);
 		
 		txtPrecio = new JTextField();
-		txtPrecio.setBounds(89, 358, 86, 20);
+		txtPrecio.setBounds(89, 373, 86, 25);
 		getContentPane().add(txtPrecio);
 		txtPrecio.setColumns(10);
 		
-		btnAgregar = new JButton("Agregar");
-		btnAgregar.setBounds(544, 94, 89, 23);
+		btnAgregar = new JButton(" Agregar");
+		btnAgregar.setIcon(new ImageIcon(ABMComboVista.class.getResource("/Iconos/Agregar.png")));
+		btnAgregar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnAgregar.setBounds(544, 206, 140, 40);
 		getContentPane().add(btnAgregar);
 		
-		btnQuitar = new JButton("Quitar");
-		btnQuitar.setBounds(544, 128, 89, 23);
+		btnQuitar = new JButton(" Quitar");
+		btnQuitar.setIcon(new ImageIcon(ABMComboVista.class.getResource("/Iconos/Quitar.png")));
+		btnQuitar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnQuitar.setBounds(544, 257, 140, 40);
 		getContentPane().add(btnQuitar);
 		
 		btnGuardar = new JButton("Guardar");
-		btnGuardar.setBounds(445, 389, 89, 23);
+		btnGuardar.setIcon(new ImageIcon(ABMComboVista.class.getResource("/Iconos/Guardar.png")));
+		btnGuardar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnGuardar.setBounds(394, 373, 140, 40);
 		getContentPane().add(btnGuardar);
 		
-		btnCancelar = new JButton("Cancelar");
-		btnCancelar.setBounds(544, 389, 89, 23);
+		btnCancelar = new JButton(" Cancelar");
+		btnCancelar.setIcon(new ImageIcon(ABMComboVista.class.getResource("/Iconos/salir.png")));
+		btnCancelar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnCancelar.setBounds(544, 373, 140, 40);
 		getContentPane().add(btnCancelar);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(ABMComboVista.class.getResource("/Iconos/Logo Pizzeria Wild .png")));
+		lblNewLabel.setBounds(521, 8, 163, 167);
+		getContentPane().add(lblNewLabel);
 
 	}
 	public void Open() {
