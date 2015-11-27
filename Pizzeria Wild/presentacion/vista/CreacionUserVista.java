5861d00c4515e42f849200dcca1c2a608883004e
ESTEBANRIVERA1977 <ESTEBANRIVERA77@GMAIL.COM>
2015-11-27 21:08:29 -0200

    Actualizacion Vista creacion Ususario

--- Pizzeria Wild/presentacion/vista/CreacionUserVista.java
+++ Pizzeria Wild/presentacion/vista/CreacionUserVista.java
@@ -7,6 +7,9 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JLabel;
+import java.awt.Toolkit;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class CreacionUserVista extends JFrame {
 
@@ -20,48 +23,56 @@ public class CreacionUserVista extends JFrame {
 	 * Create the frame.
 	 */
 	public CreacionUserVista() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 337, 253);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(CreacionUserVista.class.getResource("/Iconos/pizza_1.PNG")));
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setBounds(100, 100, 463, 326);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		btnGuardar = new JButton("Guardar");
-		btnGuardar.setBounds(117, 185, 89, 23);
+		btnGuardar = new JButton(" Guardar");
+		btnGuardar.setIcon(new ImageIcon(CreacionUserVista.class.getResource("/Iconos/Guardar.png")));
+		btnGuardar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnGuardar.setBounds(297, 237, 140, 40);
 		contentPane.add(btnGuardar);
 
 		txtUsuario = new JTextField();
-		txtUsuario.setBounds(163, 61, 86, 20);
+		txtUsuario.setBounds(176, 96, 148, 25);
 		contentPane.add(txtUsuario);
 		txtUsuario.setColumns(10);
 
 		txtContra1 = new JPasswordField();
-		txtContra1.setBounds(163, 92, 86, 20);
+		txtContra1.setBounds(176, 132, 148, 25);
 		contentPane.add(txtContra1);
 
 		txtContra2 = new JPasswordField();
-		txtContra2.setBounds(163, 123, 86, 20);
+		txtContra2.setBounds(176, 168, 148, 25);
 		contentPane.add(txtContra2);
 		
 		JLabel lblBienvenidoAPizzeria = new JLabel("Bienvenido a Pizzeria Wild");
-		lblBienvenidoAPizzeria.setBounds(87, 11, 132, 14);
+		lblBienvenidoAPizzeria.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblBienvenidoAPizzeria.setBounds(112, 11, 274, 35);
 		contentPane.add(lblBienvenidoAPizzeria);
 		
 		JLabel lblPorFavorIngrese = new JLabel("Por favor ingrese su usuario y contrase\u00F1a para la base de datos:");
-		lblPorFavorIngrese.setBounds(10, 36, 319, 14);
+		lblPorFavorIngrese.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblPorFavorIngrese.setBounds(10, 57, 427, 35);
 		contentPane.add(lblPorFavorIngrese);
 		
 		JLabel lblUsuario = new JLabel("Usuario:");
-		lblUsuario.setBounds(107, 64, 46, 14);
+		lblUsuario.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblUsuario.setBounds(112, 99, 55, 25);
 		contentPane.add(lblUsuario);
 		
 		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
-		lblContrasea.setBounds(87, 95, 66, 14);
+		lblContrasea.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblContrasea.setBounds(87, 132, 79, 25);
 		contentPane.add(lblContrasea);
 		
 		JLabel lblRepitaLaContrasea = new JLabel("Repita la contrase\u00F1a:");
-		lblRepitaLaContrasea.setBounds(44, 126, 109, 14);
+		lblRepitaLaContrasea.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblRepitaLaContrasea.setBounds(44, 166, 130, 25);
 		contentPane.add(lblRepitaLaContrasea);
 	}
 
